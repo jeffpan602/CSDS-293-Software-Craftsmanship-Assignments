be48799dbf5e5d5844758b5801d3ea6be0583aeb
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-01-29 22:54:50 -0500

    Update Direction.java

--- Assignment2/src/polygon/Direction.java
+++ Assignment2/src/polygon/Direction.java
@@ -1,15 +1,37 @@
 package polygon;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 enum Direction {
     LEFT("LEFT"), RIGHT("RIGHT"), BOTTOM("BOTTOM"), TOP("TOP");
 
     // Direction property indicating if the direction is horizontal (LEFT or RIGHT)
-    private static boolean horizontal;
+    private boolean horizontal;
     // Direction property indicating if the direction is increases the coordinate value (TOP or RIGHT)
-    private static boolean increment;
+    private boolean increment;
+    static final Set<Direction> HORIZONTAL_BOUNDS = new HashSet<>(Arrays.asList(Direction.LEFT, Direction.RIGHT));
+    static final Set<Direction> VERTICAL_BOUNDS = new HashSet<>(Arrays.asList(Direction.BOTTOM, Direction.TOP));
     private final String direction;
     private Direction(String direction) {
         this.direction = direction;
+        switch(direction) {
+            case "LEFT":
+                this.horizontal = true;
+                this.increment = false;
+                break;
+            case "RIGHT":
+                this.horizontal = true;
+                this.increment = true;
+                break;
+            case "TOP":
+                this.horizontal = false;
+                this.increment = true;
+                break;
+            case "BOTTOM":
+                this.horizontal = false;
+                this.increment = false;
+                break;
+        }
     }
 
 }
