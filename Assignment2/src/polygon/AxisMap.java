0aabe0672c26c4728701918e4da96bad92595620
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-02-01 00:03:24 -0500

    Create AxisMap.java

---
+++ Assignment2/src/polygon/AxisMap.java
@@ -0,0 +1,74 @@
+package polygon;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public final class AxisMap<S> {
+
+    private Map<S, Integer> index;
+
+    private AxisMap(Collection<S> coordinates) {
+        this.index = index(coordinates);
+    }
+
+    /**
+     * Returns the indices of input coordinates sorted by associated value
+     * @param coordinates
+     * @return Map of indices and the associated values sorted
+     */
+    private Map<S, Integer> index(Collection<S> coordinates) {
+
+        Map<S, Integer> indexes = new HashMap<>();
+
+        coordinates.stream().sorted();
+        int i = 0;
+        for(S coordinate: coordinates) {
+            indexes.put(coordinate, i);
+        }
+        return indexes;
+    }
+
+    /**
+     * @param coordinates
+     * @return a  new  AxisMap  starting  from  the  given  coordinates
+     * @param <S>
+     */
+     static <S extends Comparable<S>> AxisMap from(Collection<S> coordinates) {
+        return new AxisMap(coordinates);
+    }
+
+    /**
+     * @param value
+     * @return the index of the given value, or null if the value does not have an index
+     */
+    Integer flatIndexOf(S value) {
+        return this.index.get(value);
+    }
+
+    /**
+     * Returns the number of points in the index
+     * @return size of the index of the AxisMap as an int
+     */
+    public int size() {
+        return this.index.size();
+    }
+
+    /**
+     * Returns the index of the given value
+     * @param value
+     * @return index of the input value as an int or null if value is null or value has no index
+     */
+    public Optional<Integer> indexOf(S value) {
+
+        Optional<Integer> index = Optional.of(flatIndexOf(value));
+        if(index.isPresent()) {
+            return index;
+        }
+        else {
+            return null;
+        }
+
+    }
+}
