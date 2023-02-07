ee6c0c157efc5a5725410b02d6ec24c899810f43
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-02-07 15:15:29 -0500

    Added testing

---
+++ Assignment2/src/polygon/Assignment3Tester.java
@@ -0,0 +1,16 @@
+package polygon;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Assignment3Tester {
+
+    @Test
+    public void testIndexPairIncrement() {
+
+        //IndexPair coordinate = new IndexPair(1, 1);
+        assertTrue(1==1);
+    }
+}
+
+
