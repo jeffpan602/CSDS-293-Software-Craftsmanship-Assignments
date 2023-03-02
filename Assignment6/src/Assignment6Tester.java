625038887856c9bde1925f43743681cfaa1fc8af
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-02 15:31:37 -0500

    sorting testing

---
+++ Assignment6/src/Assignment6Tester.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class Assignment6Tester {
+
+    @Test
+    public void testAlmostSortedListSort() {
+        List<Integer> list1 = new LinkedList<>(Arrays.asList(2, 1, 3, 5, 4, 7, 6));
+        List<Integer> sortedList1 = Assignment6.almostSortedListSort(list1, 1);
+
+        int i = 1;
+        for(Integer element: sortedList1) {
+            assertTrue(i == element);
+            i++;
+        }
+    }
+}
