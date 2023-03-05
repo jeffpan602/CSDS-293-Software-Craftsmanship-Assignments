42010d7b4e3be865d323025fa6c47943fb661f85
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-05 17:16:36 -0500

    added testing

--- Assignment6/src/Assignment6Tester.java
+++ Assignment6/src/Assignment6Tester.java
@@ -17,12 +17,29 @@ public class Assignment6Tester {
             assertTrue(i == element);
             i++;
         }
+
+        List<Integer> list2 = new LinkedList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+        List<Integer> sortedList2 = Assignment6.almostSortedListSort(list2, 9);
+
+        int j = 1;
+        for(Integer element: sortedList2) {
+            assertTrue(j == element);
+            j++;
+        }
     }
     public static void main(String[] args) {
         List<Integer> list1 = new LinkedList<>(Arrays.asList(2, 1, 3, 5, 4, 7, 6));
         List<Integer> sortedList1 = Assignment6.almostSortedListSort(list1, 1);
 
-        System.out.println(list1);
-        System.out.println(sortedList1);
+        System.out.println("List 1: " + list1);
+        System.out.println("Sorted List 1: " + sortedList1);
+
+        System.out.println();
+
+        List<Integer> list2 = new LinkedList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+        List<Integer> sortedList2 = Assignment6.almostSortedListSort(list2, 9);
+
+        System.out.println("List 2: " + list2);
+        System.out.println("Sorted List 2: " + sortedList2);
     }
 }
