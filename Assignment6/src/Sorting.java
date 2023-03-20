365a2c165c25a187b7b16f9daea0e433120a2b17
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-19 22:57:34 -0400

    fixed hw comments

--- Assignment6/src/Sorting.java
+++ Assignment6/src/Sorting.java
@@ -1,9 +1,9 @@
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.List;
 
-public class Assignment6 {
-
+public class Sorting {
     /**
      * Sorting algorithm to sort a given list that is almost sorted with a distortion factor of k
      * The value of k denotes how close the list is to be fully sorted where the ith smallest element is somewhere between index i-k and i+k
@@ -15,31 +15,39 @@ public class Assignment6 {
      * @param <E> generic type for values in the list
      */
     public static <E extends Comparable<? super E>>  List<E> almostSortedListSort(List<E> list, int k) {
+        //error checking
+        Objects.requireNonNull(list);
+        Sorting.verifyPreconditions(list, k);
 
-        Assignment6.checkForPreconditions(list, k);
+        //let pq be a new priority queue
+        PriorityQueue<E> queue = new PriorityQueue<>();
 
-        PriorityQueue<E> pq = new PriorityQueue<>();
+        //insert first k elements into the queue
+        queue.addAll(list.subList(0,k+1));
 
-        for(int i = 0; i <= k; i++) {
-            pq.add(list.get(i));
-        }
-
+        //let sortedList be a new list
         List<E> output = new LinkedList<>();
 
+        //insert remaining elements from list into the queue
+        //remove head of priority queue and add to the output list
         for(int i = k+1; i < list.size(); i++) {
-            pq.add(list.get(i));
-            output.add(pq.remove());
+            queue.add(list.get(i));
+            output.add(queue.remove());
         }
 
-        while(!pq.isEmpty()) {
-            output.add(pq.remove());
+        //remove remaining elements of priority queue and add to output list
+        while(!queue.isEmpty()) {
+            output.add(queue.remove());
         }
-
+        //return output list as sorted list
         return output;
     }
     //helper method to check preconditions
-    private static <E extends Comparable<? super E>> void checkForPreconditions(List<E> list, int k) {
-        if(k < 0 || k > 100) {
+    private static <E extends Comparable<? super E>> void verifyPreconditions(List<E> list, int k) {
+        if(Objects.isNull(list)) {
+            throw new IllegalArgumentException("Input list must be non-null");
+        }
+        if(k < 0 || 100 < k)  {
             throw new IllegalArgumentException("Distortion factor k must be at most 100");
         }
         for(E element: list) {
