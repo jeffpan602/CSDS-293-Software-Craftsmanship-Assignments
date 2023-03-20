7b8052a03361225bbb299ddd5433150c4a8f00d4
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-03-20 03:29:07 -0400

    Update SortingTester.java

--- Assignment6/src/SortingTester.java
+++ Assignment6/src/SortingTester.java
@@ -9,29 +9,23 @@ public class SortingTester {
 
     @Test
     public void testAlmostSortedListSort() {
+        //Good Data Test (Legacy Test)
         List<Integer> list1 = new LinkedList<>(Arrays.asList(2, 1, 3, 5, 4, 7, 6));
         List<Integer> sortedList1 = Sorting.almostSortedListSort(list1, 1);
 
         int i = 1;
         for(Integer element: sortedList1) {
-            assertTrue(i == element);
+            assertEquals(i, (int) element);
             i++;
         }
-
+        //Good Data Test (Legacy Test)
         List<Integer> list2 = new LinkedList<>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
         List<Integer> sortedList2 = Sorting.almostSortedListSort(list2, 9);
 
         int j = 1;
         for(Integer element: sortedList2) {
-            assertTrue(j == element);
+            assertEquals(j, (int) element);
             j++;
         }
-    }
-    public static void main(String[] args) {
-        List<Integer> list1 = new LinkedList<>(Arrays.asList(2, 1, 3, 5, 4, 7, 6));
-        List<Integer> sortedList1 = Sorting.almostSortedListSort(list1, 1);
-
-        System.out.println("List 1: " + list1);
-        System.out.println("Sorted List 1: " + sortedList1);
     }
 }
